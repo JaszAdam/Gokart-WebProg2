e9ff7c2debf52b64c013022f0bfd4e31bc5bab36
JaszAdam <jaszo.adam@gmail.com>
2025-04-18 20:31:03 +0200

    feat: added a header and a dropdown menuto the project

---
+++ Gokart-WebApp/src/lib/components/Header.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+    import logo from '$lib/logo0.png';
+    import BurgerMenu from './BurgerMenu.svelte';
+
+    let MenuOpen = false;
+
+    const ToggleMenu = () =>{
+        MenuOpen = !MenuOpen;
+    };
+</script>
+
+<header>
+    <div class= "header">
+        <div class = "center">
+            <a href = "/">
+                <img src={logo} alt="Gokart"/>
+            </a>
+            <nav class={MenuOpen ? 'open' : ''}>
+                <ul>
+                    <li><a href= "/AboutUs">Rólunk</a></li>
+                    <li><a href= "/RangList">Ranglista</a></li>
+                    <li><a href= "/Booking">Időpontfoglalás</a></li>
+                    <li><a href= "/Rating">Vélemény</a></li>
+                </ul>
+            </nav>    
+        </div>
+        <div class= "right">
+            <BurgerMenu {MenuOpen} {ToggleMenu} />
+        </div>
+    </div>
+</header>
+
+
+
+<style>
+    .header{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 1rem 2rem;
+        background-color: rgb(204, 226, 240);
+        box-shadow: 0 0 10px 0;
+    }
+    .center{
+        gap: 2rem;
+        display: flex;
+        align-items: center;
+    }
+    nav ul{
+        list-style: none;
+        display: flex;
+        gap: 2.5rem;
+        padding: 0;
+        margin: 0;
+    }
+    nav a{
+        text-decoration: none;
+        color: black;
+        font-family: 'Courier New', Courier, monospace;
+        font-weight: bold;
+        font-size: large;
+    }
+    nav a:hover{
+        color: rgb(58, 58, 58);
+    }
+    :global(body) {
+        margin: 0;
+    }
+    img{
+        width: 140px;
+    }
+
+    @media(max-width: 768px){
+        nav{
+            overflow: hidden;
+            max-height: 0;
+            opacity: 0;
+            transition: all 0.3s ease;
+            flex-direction: column;
+            position: absolute;
+            top: 80px;
+            left: 0;
+            right: 0;
+            background-color: rgb(204, 226, 240);
+            padding: 0 1rem;
+            z-index: 10;
+        }
+        nav.open{
+            display: flex;
+            opacity: 1;
+            padding: 1rem;
+            max-height: 400px;
+        }
+        nav ul{
+            flex-direction: column;
+            gap: 1rem;
+        }
+        .center{
+            flex: 1;
+            justify-content: space-between;
+            width: 100%;
+        }
+    }
+</style>
