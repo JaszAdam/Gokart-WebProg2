a5a3f2a2e573fa822c2b31eab8f83226010b057e
JaszAdam <jaszo.adam@gmail.com>
2025-04-19 16:38:37 +0200

    feat: added ranglist component

--- Gokart-WebApp/src/routes/ranglist/+page.svelte
+++ Gokart-WebApp/src/routes/ranglist/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import Header from "$lib/components/Header.svelte";
+    import Ranglist from "$lib/components/Ranglist.svelte";
 </script>
 
 <Header />
 <h1>Ranglista</h1>
+<Ranglist />
